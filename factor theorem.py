1c3f65c8077e0fac471003ca43383207d03932c0
AshvinKooner <ashvinkooner@gmail.com>
2023-07-14 20:36:12 +0100

    moved functions to module, added validation, uses algebra input

--- factor theorem.py
+++ factor theorem.py
@@ -1,32 +1,30 @@
-def get_expression(highest_degree : int):
-    coeff = []
-    for i in range(highest_degree, -1, -1):
-        if i == 0:
-            msg = "Enter the constant: "
-        elif i == 1:
-            msg = "Enter coefficient of x: "
-        else:
-            msg = f"Enter coefficient of x^{i}: "
-        coeff.append(int(input(msg)))
-    return coeff
+from funcs import get_expr, get_order
 
 # Get main expression (test if factor of this)
-print("****Enter details of the expression****")
-highest_degree_exp = int(input("Enter the degree of the highest power: "))
-coeff_exp = get_expression(highest_degree_exp)
+print("****Expression****")
+expression = get_expr()
 
-# Get factor to test and find solution to factor = 0
-print("****Enter details of the factor****")
-highest_degree_fac = 1
-coeff_fac = get_expression(highest_degree_fac)
-solution = (coeff_fac[1] / coeff_fac[0]) * -1
-print(solution)
+# Get factor to test and find solution to factor = 0. Factor must be linear.
+valid = False
+while not valid:
+    print("****Factor****")
+    factor = get_expr()
+    if get_order(factor) == 1:
+        valid = True
+    else:
+        print("Factor must be linear")
+solution = -(factor[1][0] / factor[0][0])
+#print(solution)
 
 # Test this solution in the expression
 total = 0
-for i in range(len(coeff_exp)):
-    coeff = coeff_exp[i]
-    power = highest_degree_exp - i
+for term in expression:
+    coeff = term[0]
+    power = term[1]
     total += coeff * (solution**power)
-print(total)
+
+if total == 0:
+    print("It is a factor")
+else:
+    print("Not a factor. Remainder:", total)
 
